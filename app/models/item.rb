a458ae5d23a0128c60901e6f1bcfcbf4a0bb5bd5
chiapini <chiara.rei.filippini@gmail.com>
2022-05-26 20:38:08 +0900

    ActiveHashを用いた実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   validates  :name, :information, :category_id, :situation_id, :delivery_id, :area_id, :jour_id, :price, :user, presence: true
 
   belongs_to :user
+
+  class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category, :situation, :delivery, :area, :jour
+  end
+
+  validates :category_id,:situation_id,:delivery_id,:area_id,:jour_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+
 end
