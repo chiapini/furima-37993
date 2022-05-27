ff96133098dc10bd76cbdbd23894d1f893546893
chiapini <chiara.rei.filippini@gmail.com>
2022-05-27 14:49:13 +0900

    販売手数料・利益率の計算式

---
+++ app/javascript/price.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  const price = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  price.addEventListener('input',() => {
+    const inputValue = price.value;
+    tax.innerHTML = Math.floor(inputValue * 0.1)
+    profit.innerHTML = Math.floor(inputValue-tax.innerHTML)
+  })
+});
