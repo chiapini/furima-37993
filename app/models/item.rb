2ce79ba2f98df2691219084d9b54743992924c5d
chiapini <chiara.rei.filippini@gmail.com>
2022-05-27 18:42:07 +0900

    rubocopによる修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :situation
-    belongs_to :delivery 
-    belongs_to :area
-    belongs_to :jour
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :delivery
+  belongs_to :area
+  belongs_to :jour
 
   validates :category_id, :situation_id, :delivery_id, :area_id, :jour_id,
             numericality: { other_than: 1, message: "can't be blank" }
