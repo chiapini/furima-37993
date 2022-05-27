75004a0b0462ad4dcda49ec1d9f9964713d3ed81
chiapini <chiara.rei.filippini@gmail.com>
2022-05-27 15:31:50 +0900

    Rubocopによる修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
     belongs_to :category, :situation, :delivery, :area, :jour
   end
 
-  validates :category_id, :situation_id, :delivery_id, :area_id, :jour_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, :situation_id, :delivery_id, :area_id, :jour_id,
+            numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'は半角数値で設定してください' }
   validates :price,
             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::name[1..40]}
-    information { Faker::Lorem.characters(number: 100)}
-    category_id  { Faker::Number.between(from: 2, to: 11) }
-    situation_id  { Faker::Number.between(from: 2, to: 7) }
-    delivery_id  { Faker::Number.between(from: 2, to: 3) }
+    name { Faker.name[1..40] }
+    information { Faker::Lorem.characters(number: 100) }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    situation_id { Faker::Number.between(from: 2, to: 7) }
+    delivery_id { Faker::Number.between(from: 2, to: 3) }
     area_id  { Faker::Number.between(from: 2, to: 48) }
     jour_id  { Faker::Number.between(from: 2, to: 4) }
-    price { Faker::Number.between(from: 300, to: 9999999)}
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
 
     after(:build) do |item|
