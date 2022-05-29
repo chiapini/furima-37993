7c033771a6037d51d6a06292bfd16c0e8ad04da7
chiapini <chiara.rei.filippini@gmail.com>
2022-05-29 11:39:59 +0900

    Formオブジェクトパターンの枠組み作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,4 +3,18 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def new
+    @purchase_adress = PurchaseAdress.new (purchase_adress_params)
+    if  @purchase_adress.valid?
+      @purchase_adress.save
+      redirect_to root_path     
+   else
+     render :new
+  end
+end
+
+  private
+  def purchase_adress_params
+    params.require(:PurchaseAdress).permit(:post_code,:area_id,:building,:municipality,:adress,:phone).merge
+  end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,4 @@
 class Purchase < ApplicationRecord
-  validates  :post_code,:area_id,:municipality,:adress,:phone,:purchase, presence: true
-
-
   belongs_to :user
   belongs_to :item
   has_one :address
