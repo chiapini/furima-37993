0e87f8a073ba782964035163b7e5c799be92f925
chiapini <chiara.rei.filippini@gmail.com>
2022-05-31 11:43:31 +0900

    モデルの単体テストコード

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,8 +2,8 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :post_code, :area_id, :building, :municipality, :address, :phone, :purchase_id, :user_id, :item_id, :token
 
-  validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: '「3桁ハイフン4桁」の半角文字列で設定してください' }
-  validates :phone, format: { with: /\A\d{10,11}\z/, message: '電話番号は、10桁以上11桁以内の半角数値で設定してください' }
+  validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は、「3桁ハイフン4桁」の半角文字列で設定してください' }
+  validates :phone, format: { with: /\A\d{10,11}\z/, message: 'は、10桁以上11桁以内の半角数値で設定してください' }
   validates :post_code,:municipality, :address, :phone, :user_id, :item_id,:token, presence: true
   validates :area_id,numericality: { other_than: 1, message: "can't be blank" }
 
