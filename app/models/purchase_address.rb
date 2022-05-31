d8d344850ddc47e2ad68826caddb660d882cacf6
chiapini <chiara.rei.filippini@gmail.com>
2022-05-31 20:02:32 +0900

    issue9　商品購入機能の修正１

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :building, :municipality, :address, :phone, :purchase_id, :user_id, :item_id, :token
+  attr_accessor :post_code, :area_id, :building, :municipality, :address, :phone, :user_id, :item_id, :token
 
   validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'は、「3桁ハイフン4桁」の半角文字列で設定してください' }
   validates :phone, format: { with: /\A\d{10,11}\z/, message: 'は、10桁以上11桁以内の半角数値で設定してください' }
